a31689874c66d456ba891d2f35c11bb16a3f4452
kadoexec <ricardocrescencio1666@gmail.com>
2025-10-19 23:51:34 -0300

    feat: enhance HomePage layout with styled text and floating action button

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,7 +7,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Streakly'), centerTitle: true),
-      body: const Center(child: Text('Nenhum hábito ainda')),
+      body: const Center(
+        child: Text('Nenhum hábito ainda', style: TextStyle(fontSize: 18)),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('Adicionar Hábito'),
+        icon: const Icon(Icons.add),
+      ),
     );
   }
 }
